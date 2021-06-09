66b6c6970f774ecf1f386a3dc3f50366d971696d
Sebastien <seb@meridium.ca>
2021-06-08 21:17:57 -0400

    Use encoded mysql credentials

--- restorecpanelaccount.sh
+++ restorecpanelaccount.sh
@@ -1,31 +1,29 @@
 #!/bin/bash
+# version 1.5 < 2021/06/03: Use encoded credentials for mysql access
 # version 1.4 < 2014/03/18: Last version for cPanel legacy backups 
-# version 1.3 < 2013/09/30: Apache & Network configuration for restoring account on Ubuntu Local/Virtual machine/
+# version 1.3 < 2013/09/30: Apache & Network configuration for restoring account on Ubuntu Vagrant-VM >
 # version 1.2 < 2013/07/09: /homedir/ no longuer tared in cpanel daily backups >
-# version 1.1 < 2012/09/29: keep complete account homedir content for handling external webtree content (e.g. data dir)
+# version 1.1 < 2012/09/29: keep complete account homedir content for handling external webtree content (e.g. data dir) >
+
+###################################################################################
+#  This script is optimized for restoration of a cPanel backup on an Ubuntu VM.
 #
-# Usefull script to restore cPanel daily or manual backups on a local Ubuntu virtual machine
-# in order to test the integrity of the backups from time to time (you never know!) or
-# to dump a copy of your website in a local environment for testing purposes.
-# The account will be restored to default Apache web root
+# (c) Meridium Technologies, 2012-2021
 #
-# Usage: ./restorecpanelaccounts.ssh backup-file-name
-#
-# NOTE: You must be familiar with local/home/office tlds for internal DNS resolution.
-#
-# WARNING: DO NOT USE THIS SCRIPT ON A CPANEL SERVER OR A PRODUCTION SERVER.
-#
-# (c) Meridium Technologies, 2012-2019
+#  WARNING: DO NOT USE THIS SCRIPT ON A CPANEL SERVER OR A PRODUCTION SERVER.#
 #
 ###################################################################################
 
-#### Configuration ####
+# NOTE: Use the following command to encode your mysql credentials on your vm
+#   mysql_config_editor set --login-path=local --host=localhost --user=root --password
 
-archive=$1        # pass cPanel archive to variable
-mysqluser='xxxxx'  # mysql username to use for restore
-mysqlpwd='xxxx' # set MySQL root password
-fileown='xxxx.xxxx' # owner.group
-tld='xxxx'
+#### Settings ####
+archive_name=$1        	  # pass cPanel archive_name to variable
+archive_dir='' # no trailing slash
+tld=''		          # local domain on VM
+fileown='user:group' 	  # owner.group
+conn='--login-path=local' # credentials to access mysql engine
+httpdconfpath='/etc/apache2'
 
 #### Do not edit below ###
 
@@ -40,24 +38,15 @@ function usage ()
  exit 1 #exit shell script
 }
 
-# display trailing dot while unpacking homedir.tar
-function progress(){
-while true
-do
-     echo -n "."
-     sleep 1
-done
-}
-
 # Extract cPanel account name
 # check if manual/automated cPanel backup
-if [[ "$archive" == *backup* ]]
+if [[ "$archive_name" == *backup* ]]
 then
-		uncompressedDir=${archive%%.tar.gz}
+		uncompressedDir=${archive_name%%.tar.gz}
         accountname=$(echo $uncompressedDir | cut -d'_' -f3)
 		echo "Launching cPanel Account Name => $accountname"
 else
-		accountname=${archive%%.tar.gz}
+		accountname=${archive_name%%.tar.gz}
         uncompressedDir=$accountname
 		echo "Launching cPanel Account Name => $accountname"
 fi
@@ -65,12 +54,12 @@ fi
 ### SANITY CHECKS ###
 
 # make sure that file exists
-if [ ! -f /home/seb/public/$archive ]; then
-  usage "Invalid argument! (the file /home/seb/public/$archive does not exist in this directory)"
+if [ ! -f $archive_dir/$archive_name ]; then
+  usage "Invalid argument! (the file $archive_dir/$archive_name does not exist in this directory)"
 fi
 
 # Make sure this is a true cPanel archive (skipped because taking to long)
-# tar -tzf $archive | grep 'version'
+# tar -tzf $archive_name | grep 'version'
 
 # check if the account already exists on the local server
 if [ -d /var/www/$accountname ]; then
@@ -85,26 +74,26 @@ read fanswer
 	echo " Done!"
 
 	# Delete local mysql databases if they exist (fresh install)
-	databases=(`echo 'show databases;' | mysql -u$mysqluser -p$mysqlpwd | grep $accountname\_`)
+	databases=(`echo 'show databases;' | mysql $conn | grep $accountname\_`)
 	count=(`echo ${#databases[@]}`)
 	if [ $count -gt 0 ]; then
 		echo "=> Deleting existing account databases ($count found!): "
 		for d in "${databases[@]}"; do
-			mysqladmin -u$mysqluser -p$mysqlpwd -f drop ${d}
+			mysqladmin $conn -f drop ${d}
 		done
 	else
 		echo "No local database found for the account $accountname."
 	 fi
 	# Delete local mysql users
-	dbusers=(`echo "SELECT CONCAT(User,'@',Host) FROM mysql.user WHERE User LIKE '%$accountname%';" | mysql -u$mysqluser -p$mysqlpwd | grep $accountname`)  
+	dbusers=(`echo "SELECT CONCAT(User,'@',Host) FROM mysql.user WHERE User LIKE '%$accountname%';" | mysql $conn | grep $accountname`)  
 	ucount=(`echo ${#dbusers[@]}`)
 	if [ $ucount -gt 0 ]; then
 		echo "=> Dropping local mysql users & privileges ($ucount found!): "
 		for u in "${dbusers[@]}"; do
-			mysql -u$mysqluser -p$mysqlpwd -e "DROP USER $u;"
+			mysql $conn -e "DROP USER $u;"
 			echo "user $u dropped!"
 		done
-	mysql -u$mysqluser -p$mysqlpwd -e "FLUSH PRIVILEGES;"
+	mysql $conn -e "FLUSH PRIVILEGES;"
 	else
 		echo "No matching mysql user found!"
 	fi
@@ -116,16 +105,8 @@ fi
 ###### UNTAR PACKAGE ######
 echo -e "\E[32m== RESTORING ACCOUNT $accountname ON LOCAL SERVER =="; tput sgr0
 echo -n "=> Unpacking files and database informations to /tmp/$uncompressedDir. Please wait..."
-
- # Start it in the background
- #progress &
- # Save PID
- #MYSELF=$!
- # cp /vagrant/src/$archive /tmp/$archive
- tar -C /tmp/ -xzf /home/seb/public/$archive $uncompressedDir/homedir $uncompressedDir/mysql.sql $uncompressedDir/mysql
- # Kill progress
- #kill $MYSELF >/dev/null 2>&1
- echo "...Done!"
+tar -C /tmp/ -xzf /home/seb/$archive_dir/$archive_name $uncompressedDir/homedir $uncompressedDir/mysql.sql $uncompressedDir/mysql
+echo "...Done!"
 
 echo "=> Creating directory /var/www/$accountname"
 mkdir /var/www/$accountname
@@ -150,15 +131,15 @@ for filename in /tmp/$uncompressedDir/mysql/$accountname\_*.sql; do
  DB=$(echo $filename | cut -d'/' -f5 | cut -d'.' -f1)
  echo -n "=> Creating and importing database -> $DB..."
  CMD1="CREATE database IF NOT EXISTS $DB;"
- mysql -u$mysqluser -p$mysqlpwd -e "$CMD1"
- mysql -u$mysqluser -p$mysqlpwd $DB < $filename
+ mysql $conn -e "$CMD1"
+ mysql $conn $DB < $filename
  echo " Done!"
 done
 
 #restore mysql users
 echo -n "=> Creating mysql users & manage privileges..."
 GRANT1=$(cat /tmp/$uncompressedDir/mysql.sql | grep 'localhost')
-mysql -u$mysqluser -p$mysqlpwd -e "$GRANT1"
+mysql $conn -e "$GRANT1"
 echo " Done!"
 
 echo -n "=> Removing temp account files ("
@@ -166,8 +147,7 @@ echo -n "/tmp/$uncompressedDir)..."
 rm -rf  /tmp/$uncompressedDir/
 echo " Done!"
 
-# Configuration de apache
-httpdconfpath='/etc/apache2'
+# Configure apache ( ubuntu )
 httpdvhostconf="$httpdconfpath/sites-available/$accountname.$tld-vhost.conf"
 sitesenabled="$httpdconfpath/sites-enabled/$accountname.$tld-vhost.conf"
 
